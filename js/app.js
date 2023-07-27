dc2bef4982b5970d17734a47f74859fea58885d2
Jo-Danha <133372964+Jo-Danha@users.noreply.github.com>
2023-07-27 23:47:44 +0700

    modified:   css/app.css 	modified:   index.html 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -623,24 +623,6 @@ const modalFoto = (img) => {
     modal.show();
 };
 
-$(window).load(function() {    
-    var theWindow = $(window),
-    $bg = $("bg"),
-    aspectRatio = $bg.width() / $bg.height();	    			    		
-    function resizeBg() {		
-      if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
-        $bg
-        .removeClass()
-        .addClass('bgheight');
-      } else {
-        $bg
-        .removeClass()
-        .addClass('bgwidth');
-      }					
-    }	                   			
-    theWindow.resize(resizeBg).trigger("resize");
-  });
-
 window.addEventListener('load', () => {
     let modal = new bootstrap.Modal('#exampleModal');
     let name = (new URLSearchParams(window.location.search)).get('to') ?? '';
